f8e4d1805a33b798dac82dc100011b18cccf7c5b
Ruth C Ezeh <ruthrita45@gmail.com>
2022-06-21 12:47:23 +0100

    update on class component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,55 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'passwordGeneratingApp';
+
+  _length = 0;
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+  password = '';
+
+  onChangeLength(value: string) {
+    const parsedValue = parseInt(value);
+
+    if (!isNaN(parsedValue)) {
+      this._length = parsedValue;
+    }
+  }
+
+  onChangeUseLetters(){
+    this.includeLetters = !this.includeLetters;
+  }
+  onchangeUseNumbers(){
+    this.includeNumbers = !this.includeNumbers;
+  }
+  onchangeUseSymbols(){
+    this.includeSymbols = !this.includeSymbols;
+  }
+  onButtonClick() {
+    const numbers = "1234567890";
+    const letters = "abcdefghijklmonpqrstuvwxyz"
+    const symbols = "!@#$%^&*()";
+
+    let validChars = '';
+    if (this.includeLetters){
+      validChars += letters;
+    }
+
+    if (this.includeNumbers){
+      validChars += numbers;
+    }
+
+    if (this.includeSymbols){
+      validChars += symbols;
+    }
+
+    let generatedPassword = '';
+    for(let i = 0; i < this._length; i++) {
+      const _index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[_index];
+
+      console.log(generatedPassword);
+    }
+    this.password = generatedPassword;
+    }
 }
